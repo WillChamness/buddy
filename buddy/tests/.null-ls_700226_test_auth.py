d8f6fc908c2e83d4763a9b66da1f89cdf3ef62d4
WillChamness <wzchamness@gmail.com>
2025-03-16 20:24:55 -0400

    chore: remove null-ls file

--- buddy/tests/.null-ls_700226_test_auth.py
+++
@@ -1,7 +0,0 @@
-from buddy.tests._env import ServerSettings
-from buddy.tests.http_test import HttpTestCase
-
-class TestSignup(HttpTestCase):
-    def test_signup(self):
-        username, password = self.generate_random_user()
-        
