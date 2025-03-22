e1bd39905a103fc19c242c3e3e831b7b44b8b062
WillChamness <wzchamness@gmail.com>
2025-03-21 23:13:15 -0400

    test: add keyword argument to connection test

--- buddy/tests/test_connection.py
+++ buddy/tests/test_connection.py
@@ -3,7 +3,7 @@ from buddy.tests.http_test import HttpTestCase
 
 class TestServerConnectivity(HttpTestCase):
     def test_connection(self) -> None:
-        response: requests.Response = self.get("/")
+        response: requests.Response = self.get(path="/")
         self.assertOk(response.status_code)
         self.assertEqual(response.json(), "Buddy is running")
 
