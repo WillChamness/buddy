1d68c62aa6ec8eff5bd9ebe32834df18fd26f2c2
WillChamness <wzchamness@gmail.com>
2025-03-21 23:12:18 -0400

    refactor: use dotenv module to load development.env

--- run.py
+++ run.py
@@ -3,14 +3,9 @@ Run this file to run the debugger
 """
 import os
 import uvicorn
+import dotenv
 
-with open("development.env", "r") as f:
-    lines = f.readlines()
-
-for line in lines:
-    key, val = line.split("=")[0].strip(), line.split("=")[1].strip().replace('"', '')
-    print(f"{key}: {val}")
-    os.environ[key] = val
+dotenv.load_dotenv(dotenv_path="./development.env")
 
 from buddy.src.main import app
 uvicorn.run(app)
